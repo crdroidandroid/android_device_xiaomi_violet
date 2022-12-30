3fac68688c63c42373bdaa62da47e2357437e062
jash <jashvakharia@gmail.com>
2022-12-30 17:52:47 +0000

    violet: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+echo 'Cloning Additional Trees'
+
+# Additional Trees
+
+git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi-firmware.git vendor/xiaomi-firmware
+git clone --depth=1 https://github.com/jash69/vendor_xiaomi_violet-1 vendor/xiaomi/violet
+git clone --depth=1 https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git kernel/xiaomi/violet
