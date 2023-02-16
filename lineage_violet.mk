7e5489ec361dbfc5cf7d0744ff85ae03ffc8dc48
Jash Vakharia <jashvakharia@gmail.com>
2023-02-16 12:29:32 +0530

    violet: Build crDroid

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -9,8 +9,7 @@
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
-SUPERIOR_OFFICIAL := true
+$(call inherit-product, vendor/crdroid/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_MATLOG := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
@@ -20,7 +19,7 @@ USE_MOTO_CALCULATOR := true
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_violet
+PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
