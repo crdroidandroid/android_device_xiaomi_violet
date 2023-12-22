8ea01c4de894fc8b79f92880a457368b7f6b918e
Mohammad Kibria <106745675+kibria5@users.noreply.github.com>
2023-12-22 20:05:07 +0000

    violet: Include MiuiCamera
    Signed-off-by: kibria5 <mdkibria687@gmail.com>

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
 # Inherit some common crDroid stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP  := true
